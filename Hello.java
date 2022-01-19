6de7c2a2fe9d13d3e4aa1830c288df12e7b19b77
kkunjals <98001174+kkunjals@users.noreply.github.com>
2022-01-18 21:50:25 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/************************************************************************
+*Hello.java
+*************************************************************************
+*Kunjal Sangroula
+*01/17/2022
+*CMSC 255 C90
+*************************************************************************/
+
+public class Hello {
+	public static void main (String[] args) {
+
+		System.out.println("Kunjal Sangroula");
+		System.out.println("Hello.java");
+		System.out.println("Lab 01 Introduction to the Course");
+		System.out.println("01/17/2022");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+		
+	}
+}
+
+
